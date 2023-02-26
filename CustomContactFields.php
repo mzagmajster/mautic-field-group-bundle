90a11d80df4abba93f9a337d8eb20384e41a97f2
Matic Zagmajster <maticzagmajster@gmail.com>
2023-02-26 22:55:20 +0100

    Add some boilerplate code

---
+++ CustomContactFields.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\HelloWorldBundle;
+
+/**
+ * Mautic Custom Contact Fields
+ *
+ * Instruction: Create constant for each custom field plugin depends on. Constant should have value of custom field alias.
+*/
+final class CustomContactFields {
+
+}
